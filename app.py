bf6569cc4408af6c390fce39312c8d31b6504c98
Kismat-adhikari <kismatadhikari62@gmail.com>
2025-07-17 15:35:08 +0545

    d

--- app.py
+++ app.py
@@ -610,6 +610,8 @@ def check_answer():
         'is_correct': is_correct,
         'correct_answer': correct_answer
     })
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
